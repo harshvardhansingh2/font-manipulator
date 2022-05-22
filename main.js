83f27a3297aad14203ab75d2fb712256eb567bd4
harshvardhansingh2 <97771241+harshvardhansingh2@users.noreply.github.com>
2022-05-21 18:54:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background('#969A97');
+    textSize(difference);
+    fill('#F90093');
+    text('Harsh Vardhan Singh', 30, 200);
+    document.getElementById("font_size").innerText = "Font size of the text will be =  " + difference + "px";
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
